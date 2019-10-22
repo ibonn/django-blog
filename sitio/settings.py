a3fff1ebe9f494bb27da1f3f02aad1fc8395d04e
Ibon <ibonescartin@gmail.com>
2019-10-22 14:50:51 +0200

    Añadido host

--- sitio/settings.py
+++ sitio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$eutip7q1zjgckv*%ujw$qtx(_$e^o*rbm_)&y%2!^-d&&qt$*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ibonn.pythonanywhere.com']
 
 
 # Application definition
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
+<
 TIME_ZONE = 'Europe/Madrid'
 
 USE_I18N = True
